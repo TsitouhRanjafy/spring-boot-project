2d9646ab41244ce1eaf4c3d69971af8c1a26bcd0
TsitouhRanjafy <radohery09@gmail.com>
2026-02-25 16:21:47 +0300

    spring with cloud firestore

--- cloud-firestore/src/main/java/com/example/cloudfirestore/repository/UserRepository.java
+++ cloud-firestore/src/main/java/com/example/cloudfirestore/repository/UserRepository.java
@@ -3,9 +3,7 @@ package com.example.cloudfirestore.repository;
 import com.example.cloudfirestore.entity.User;
 import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
-import java.util.List;
 
 public interface UserRepository extends FirestoreReactiveRepository<User> {
 
