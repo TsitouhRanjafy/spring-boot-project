be8c43a1d541b69266a39c22a251394e85ad5d91
TsitouhRanjafy <radohery09@gmail.com>
2026-02-28 09:11:06 +0300

    Test: cash card controller

--- familyCard/src/test/java/com/example/familycard/FamilyCardApplicationTests.java
+++ familyCard/src/test/java/com/example/familycard/FamilyCardApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.familycard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestRestTemplate
 class FamilyCardApplicationTests {
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
-    void contextLoads() {
+    public void contextLoads(){
+        assertThat(this.restTemplate).isNotNull();
     }
 
+    @Test
+    void shouldReturnCashCardWhenDataIsSaved(){
+        ResponseEntity<String> response = this.restTemplate.getForEntity("/cashcard/99", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 }
