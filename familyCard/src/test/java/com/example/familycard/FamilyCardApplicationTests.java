9060191cbdd58378080387a9127261fe465b020c
TsitouhRanjafy <radohery09@gmail.com>
2026-03-03 21:27:04 +0300

    feat: create a cash card

--- familyCard/src/test/java/com/example/familycard/FamilyCardApplicationTests.java
+++ familyCard/src/test/java/com/example/familycard/FamilyCardApplicationTests.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.net.URI;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -45,6 +47,31 @@ class FamilyCardApplicationTests {
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         assertThat(response.getBody()).isBlank();
+    }
+
+    @Test
+    void shouldCreateANewCashCard(){
+        CashCard newCashCard = new CashCard(null, 250.0);
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        // send a 201 (Created) response containing a Location header field
+        // that provides an identifier for the primary resource created
+        // https://www.rfc-editor.org/rfc/rfc9110#section-10.2.2
+        URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+        assertThat(locationOfNewCashCard).isNotNull();
+        System.out.println("\n\n\nthe location: "+locationOfNewCashCard);
+
+        // Finally, we'll use the Location header's information to fetch the newly created CashCard.
+        ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+        Number id = documentContext.read("$.id");
+        Double amount = documentContext.read("$.amount");
+
+        assertThat(id).isNotNull();
+        assertThat(amount).isEqualTo(250.0);
     }
 }
 
