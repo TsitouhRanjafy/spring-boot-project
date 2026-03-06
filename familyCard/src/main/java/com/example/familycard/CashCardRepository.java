d3dfec6187eafd38c765322645f6133d11fa372e
TsitouhRanjafy <radohery09@gmail.com>
2026-03-06 21:05:24 +0300

    Test and Implementation of finAll cashcard

--- familyCard/src/main/java/com/example/familycard/CashCardRepository.java
+++ familyCard/src/main/java/com/example/familycard/CashCardRepository.java
@@ -1,6 +1,9 @@
 package com.example.familycard;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+
 }
